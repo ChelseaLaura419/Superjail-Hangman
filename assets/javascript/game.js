b0cfb8afa1fdb246904de89a0404d7b21d479b5f
Chelsea Laura <cdesrosiers419@gmail.com>
2018-12-13 13:05:17 -0500

    Javascript Update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,94 @@
-var wins = 0;
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+    var wins = 0;
     var losses = 0;
-    var guessNumber = 13;
-    var guessLetter = [];
+    var guessNumber = 0;
+    var maxTries = 10;
+    var currentWord;
+    var wordLettersArray = [];
+    const gameStart = false;
+    const gameFinish = false;
 
     var winsText = document.getElementById("wins-text");
     var lossesText = document.getElementById("losses-text");
     var guessesLeftText = document.getElementById("guessesleft-text");
     var guessesText = document.getElementById("guesses-text");
+    var currentWordText = document.getElementById("current-word-text");
+    var currentWordHolder = document.getElementById("current-word-holder");
 
-    document.onkeyup = function(event) {
 
-        var computerChoices = ["Warden", "Prisoners", "Alice", "Jared", "Mistress", "The Twins", "Stingray", "Hunter", "Combaticus"];
+    var computerChoices = ["warden", "alice", "mistress", "the twins", "jared", "hunter", "combaticus", "stingray", "prisoners"];
+
+    var currentWord = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    for (var i = 0; i < currentWord.length; i++) {
+
+        var newDiv = document.createElement("div");
+        currentWordText.appendChild(newDiv);
+
+        var newLetter = document.createElement("p");
+        newLetter.textContent = currentWord[i];
+        newLetter.hidden = "hidden";
+        wordLettersArray.push(newLetter);
+        newDiv.appendChild(newLetter);
+
+        var newLetterUnderscore = document.createElement("p");
+        if(currentWord[i] != " ")
+            newLetterUnderscore.textContent = "_";
+        else 
+            newLetterUnderscore.textContent = " ";
+        newDiv.appendChild(newLetterUnderscore);
+
+    }
     
-        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        var guessLetter = [];
 
-        var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+        document.onkeyup = function(event) {
+            
+            
+            var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
-        var userGuess = event.key;
 
-        if (letters.indexOf(userGuess) > -1) {
+            var userGuess = event.key;
 
-        if (userGuess === computerGuess) {
-            wins++;
-            guessNumber = 9;
-            guessLetter = [];
+            if (letters.indexOf(userGuess) > -1) {
+
+            if (userGuess === currentWord) {
+                wins++;
+                guessNumber = 10;
+                guessLetter = [];
+            }
+// write a for-loop that goes through all of the letter elements
+// if the letter is === to user input, then call .show() on that element 
+            var guessedCorrectly = false;
+            for (var i = 0; i < currentWord.length; i++) {
+                if (userGuess == currentWord[i]) {
+                    // this means they guessed correctly
+                    guessedCorrectly = true;
+                    wordLettersArray[i].hidden = "";
+                    break;
+                }
             }
 
-        if (userGuess != computerGuess) {
-            guessNumber --;
-            guessLetter.push(userGuess);
+            if(guessedCorrectly == false)
+            {
+                guessNumber--;
+                guessLetter.push(userGuess);
             }
 
-        if (guessNumber === 0) {
-            guessNumber = 9;
-            losses ++;
-            guessLetter = [];
-            }
+            if (guessNumber === 0) {
+                guessNumber = 10;
+                losses ++;
+                guessLetter = [];
+                }
 
-        winsText.textContent = "Wins: " + wins;
-        lossesText.textContent = "Losses: " + losses;
-        guessesLeftText.textContent = "Number Of Guesses Left: " + guessNumber;
-        guessesText.textContent = "Letters Guessed So Far: " + guessLetter;   
+
+            winsText.textContent = "Wins: " + wins;
+            // currentWordText.textContent = currentWord;
+            lossesText.textContent = "Losses: " + losses;
+            guessesLeftText.textContent = "Number Of Guesses Left: " + guessNumber;
+            guessesText.textContent = "Letters Guessed So Far: " + guessLetter; 
+
         };
     };
+});
